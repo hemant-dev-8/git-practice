38d84ce610faeab0c606c96701be804422d61a57
Hemant Kumar <hemantnahar.nahar@gmail.com>
2022-02-16 10:32:54 +0530

    added-loop-array-file

---
+++ loop-array-items.js
@@ -0,0 +1,12 @@
+const files = [ 'foo.txt ', '.bar', '   ', 'baz.foo' ];
+let filePaths = [];
+
+for (let file of files) {
+  const fileName = file.trim();
+  if(fileName) {
+    const filePath = `~/cool_app/${fileName}`;
+    filePaths.push(filePath);
+  }
+}
+
+// filePaths = [ '~/cool_app/foo.txt', '~/cool_app/.bar', '~/cool_app/baz.foo']
